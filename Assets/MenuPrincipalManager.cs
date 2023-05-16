4808c5a03a3b00c5df71a73c3c44bb06daf6564e
rafael <rafael.eustapinto@gmail.com>
2023-05-16 14:45:22 -0300

    corrigido

--- Assets/MenuPrincipalManager.cs
+++ Assets/MenuPrincipalManager.cs
@@ -8,7 +8,20 @@ public class MenuPrincipalManager : MonoBehaviour
     [SerializeField] private string nomeDoLevelDoJogo;
     [SerializeField] private GameObject painelMenuInicial;
     [SerializeField] private GameObject painelOpcoes;
+    [SerializeField] private GameObject painelAcessibilidades;
+    [SerializeField] private GameObject painelInfo;
+    [SerializeField] private GameObject painelHelp;
 
+    public void Start(){        
+        Menu();
+    }
+    public void Menu(){
+        painelMenuInicial.SetActive(true);
+        painelAcessibilidades.SetActive(false);
+        painelOpcoes.SetActive(false);
+        painelInfo.SetActive(false);
+        painelHelp.SetActive(false);
+    }
     public void Jogar(){
         SceneManager.LoadScene(nomeDoLevelDoJogo);
     }
@@ -20,8 +33,35 @@ public class MenuPrincipalManager : MonoBehaviour
         painelMenuInicial.SetActive(true);
         painelOpcoes.SetActive(false);
     }
+    public void AbrirAcessibilidades(){
+        painelOpcoes.SetActive(false);
+        painelAcessibilidades.SetActive(true);
+    }
+    public void FecharAcessibilidades(){
+        painelAcessibilidades.SetActive(false);
+        painelOpcoes.SetActive(true);
+    }
+    public void AbrirInfo(){
+        painelInfo.SetActive(true);
+        painelMenuInicial.SetActive(false);
+    }
+    public void FecharInfo(){
+        painelInfo.SetActive(false);
+        painelMenuInicial.SetActive(true);
+    }
+    public void AbrirHelp(){
+        painelHelp.SetActive(true);
+        painelMenuInicial.SetActive(false);
+    }
+    public void FecharHelp(){
+        painelHelp.SetActive(false);
+        painelMenuInicial.SetActive(true);
+    }
     public void SairJogo(){
-        Debug.Log("Saiu do jogo");
-        Application.Quit();
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit(); 
+        #endif
     }
 }
