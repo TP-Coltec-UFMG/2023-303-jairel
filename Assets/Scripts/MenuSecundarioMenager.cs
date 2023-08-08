01af24d3739cb49e46a7d6765b0c486777ac989c
raphhax <raphaelhenriquedev@gmail.com>
2023-08-08 13:19:54 -0300

    up

--- Assets/Scripts/MenuSecundarioMenager.cs
+++ Assets/Scripts/MenuSecundarioMenager.cs
@@ -7,9 +7,14 @@ public class MenuSecundarioMenager : MonoBehaviour
 {
     [SerializeField] private string MenuPrincipal;
     [SerializeField] private string MenuFases;
+    [SerializeField] private string Inventario;
 
     public void Voltar(){        
         SceneManager.LoadScene(0);
+    }
+
+    public void VoltarInventario(){
+        SceneManager.LoadScene(1);
     }
 
     public void AvancaFases(){
