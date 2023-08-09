1d17c507693a5d2d4338bcec917460c9c3e03d41
Rafael <2021951515@teiacoltec.org>
2023-08-09 07:57:59 -0300

    fix sound bugs

--- Assets/Scripts/MenuSecundarioMenager.cs
+++ Assets/Scripts/MenuSecundarioMenager.cs
@@ -9,16 +9,39 @@ public class MenuSecundarioMenager : MonoBehaviour
     [SerializeField] private string MenuFases;
     [SerializeField] private string Inventario;
 
-    public void Voltar(){        
+    [SerializeField] private AudioSource somAvancar;
+    [SerializeField] private AudioSource somVoltar;
+
+    public void Voltar()
+    {
+        somVoltar.Play();
+        Invoke("CarregarCenaMenuPrincipal", somVoltar.clip.length);
+    }
+
+    public void VoltarInventario()
+    {
+        somVoltar.Play();
+        Invoke("CarregarCenaInventario", somVoltar.clip.length);
+    }
+
+    public void AvancaFases()
+    {
+        somAvancar.Play();
+        Invoke("CarregarCenaFases", somAvancar.clip.length);
+    }
+
+    private void CarregarCenaMenuPrincipal()
+    {
         SceneManager.LoadScene(0);
     }
 
-    public void VoltarInventario(){
+    private void CarregarCenaInventario()
+    {
         SceneManager.LoadScene(1);
     }
 
-    public void AvancaFases(){
+    private void CarregarCenaFases()
+    {
         SceneManager.LoadScene(2);
     }
 }
- 
