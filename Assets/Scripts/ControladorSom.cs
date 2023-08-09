c5ce30cf86abd6712c074fd8692370c731d908e9
Rafael Eustaquio Pinto <102390259+rafaelrat@users.noreply.github.com>
2023-08-09 15:08:49 -0300

    botão de mute funfa

--- Assets/Scripts/ControladorSom.cs
+++ Assets/Scripts/ControladorSom.cs
@@ -8,12 +8,80 @@ using UnityEngine.Audio;
 public class ControladorSom : MonoBehaviour
 {
     public AudioMixer mixer;
+    private bool isMutedFundoMusical = false;
+    private bool isMutedEfeitosSonoros = false;
+    private float previousMusicalVolume;
+    private float previousEfeitosVolume;
 
-    public void VolumeMusical(float value){
-        mixer.SetFloat("Fundo", Mathf.Log10(value) * 20);
+    private void Start()
+    {
+        // Obtém os volumes iniciais ao iniciar o jogo
+        mixer.GetFloat("Fundo", out previousMusicalVolume);
+        mixer.GetFloat("EfeitosSonoros", out previousEfeitosVolume);
     }
 
-    public void VolumeEfeitosSonoros(float value){
-        mixer.SetFloat("EfeitosSonoros", Mathf.Log10(value) * 20);
+    public void VolumeMusical(float value)
+    {
+        if (!isMutedFundoMusical)
+        {
+            mixer.SetFloat("Fundo", Mathf.Log10(value) * 20);
+        }
     }
+
+    public void VolumeEfeitosSonoros(float value)
+    {
+        if (!isMutedEfeitosSonoros)
+        {
+            mixer.SetFloat("EfeitosSonoros", Mathf.Log10(value) * 20);
+        }
+    }
+
+    public void ToggleMuteFundoMusical()
+    {
+        isMutedFundoMusical = !isMutedFundoMusical;
+
+        if (isMutedFundoMusical)
+        {
+            MuteFundoMusical();
+        }
+        else
+        {
+            UnmuteFundoMusical();
+        }
+    }
+
+    public void ToggleMuteEfeitosSonoros()
+    {
+        isMutedFundoMusical = !isMutedFundoMusical;
+
+        if (isMutedFundoMusical)
+        {
+            MuteEfeitosSonoros();
+        }
+        else
+        {
+            UnmuteEfeitosSonoros();
+        }
+    }
+
+    private void MuteEfeitosSonoros()
+    {
+        mixer.SetFloat("EfeitosSonoros", -80);
+    }
+
+    private void MuteFundoMusical()
+    {
+        mixer.SetFloat("Fundo", -80);
+    }
+
+    private void UnmuteEfeitosSonoros()
+    {
+        mixer.SetFloat("EfeitosSonoros", previousEfeitosVolume);
+    }
+
+    private void UnmuteFundoMusical()
+    {
+        mixer.SetFloat("Fundo", previousMusicalVolume);
+    }
+
 }
