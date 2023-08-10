0c8326facae7427d4d5d0f60a11b68e4460d0f54
alvimdev <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>
2023-08-10 13:32:20 -0300

    atualizacoes de bugs e arrumando tudo

--- Assets/Scripts/ControladorSom.cs
+++ Assets/Scripts/ControladorSom.cs
@@ -8,12 +8,90 @@ using UnityEngine.Audio;
 public class ControladorSom : MonoBehaviour
 {
     public AudioMixer mixer;
+    private bool isMutedFundoMusical = false;
+    private bool isMutedEfeitosSonoros = false;
+    private float previousMusicalVolume;
+    private float previousEfeitosVolume;
 
-    public void VolumeMusical(float value){
-        mixer.SetFloat("Fundo", Mathf.Log10(value) * 20);
+    [SerializeField] private Sprite somLigadoSprite;
+    [SerializeField] private Sprite somDesligadoSprite;
+
+    [SerializeField] private Image muteImage;
+
+
+    private void Start()
+    {
+        // Obtém os volumes iniciais ao iniciar o jogo
+        mixer.GetFloat("Fundo", out previousMusicalVolume);
+        mixer.GetFloat("EfeitosSonoros", out previousEfeitosVolume);
     }
 
-    public void VolumeEfeitosSonoros(float value){
-        mixer.SetFloat("EfeitosSonoros", Mathf.Log10(value) * 20);
+    public void VolumeMusical(float value)
+    {
+        if (!isMutedFundoMusical)
+        {
+            mixer.SetFloat("Fundo", Mathf.Log10(value) * 20);
+        }
     }
+
+    public void VolumeEfeitosSonoros(float value)
+    {
+        if (!isMutedEfeitosSonoros)
+        {
+            mixer.SetFloat("EfeitosSonoros", Mathf.Log10(value) * 20);
+        }
+    }
+
+    public void ToggleMuteFundoMusical()
+    {
+        isMutedFundoMusical = !isMutedFundoMusical;
+
+        if (isMutedFundoMusical)
+        {
+            MuteFundoMusical();
+        }
+        else
+        {
+            UnmuteFundoMusical();
+        }
+    }
+
+    public void ToggleMuteEfeitosSonoros()
+    {
+        isMutedFundoMusical = !isMutedFundoMusical;
+
+        if (isMutedFundoMusical)
+        {
+            MuteEfeitosSonoros();
+        }
+        else
+        {
+            UnmuteEfeitosSonoros();
+        }
+    }
+
+    private void MuteEfeitosSonoros()
+    {
+        mixer.SetFloat("EfeitosSonoros", -80);
+        muteImage.sprite = somDesligadoSprite;
+    }
+
+    private void MuteFundoMusical()
+    {
+        mixer.SetFloat("Fundo", -80);
+        muteImage.sprite = somDesligadoSprite;
+    }
+
+    private void UnmuteEfeitosSonoros()
+    {
+        mixer.SetFloat("EfeitosSonoros", previousEfeitosVolume);
+        muteImage.sprite = somLigadoSprite;
+    }
+
+    private void UnmuteFundoMusical()
+    {
+        mixer.SetFloat("Fundo", previousMusicalVolume);
+        muteImage.sprite = somLigadoSprite;
+    }
+
 }
