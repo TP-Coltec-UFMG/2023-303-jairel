4e5cd35417d08d9edb31212a2bdaaa6a89b3e95c
Guilherme Luiz <guilhermeoluiz04@gmail.com>
2023-08-31 15:41:43 -0300

    adicionando as alterações feitas pelo guilherme luiz

--- Assets/Scripts/Boss.cs
+++ Assets/Scripts/Boss.cs
@@ -1,14 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Boss : MonoBehaviour
 {
     private Animator animator;
-    private int maxLife = 60;
+    public int maxLife;
     private int currentLife;
 
-    void Awake()
+    private void Awake()
     {
         animator = GetComponent<Animator>();
         animator.SetBool("mexe", true);
@@ -20,18 +18,13 @@ public class Boss : MonoBehaviour
         if (other.CompareTag("tiro"))
         {
             Destroy(other.gameObject);
-
             currentLife--;
 
-            if (currentLife <= 0){
+            if (currentLife <= 0)
                 Destroy(gameObject);
-                GameManager.instance.showWin();
-            }
         }
 
         if (other.CompareTag("barrera"))
-        {
             GameManager.instance.FinalizarJogo();
-        }
     }
 }
--- Assets/Scripts/MenuSecundarioMenager.cs
+++ Assets/Scripts/MenuSecundarioMenager.cs
@@ -8,45 +8,39 @@ public class MenuSecundarioMenager : MonoBehaviour
 
     [SerializeField] private AudioSource somAvancar;
     [SerializeField] private AudioSource somVoltar;
-
-    public void Voltar()
+    private void Update()
+{
+    // Escutar as teclas de seta
+    if (Input.GetKeyDown(KeyCode.LeftArrow))
+    {
+        VoltarMenuPrincipal();
+    }
+    else if (Input.GetKeyDown(KeyCode.RightArrow))
+    {
+        AvancaFases();
+    }
+}
+    public void VoltarMenuPrincipal()
     {
         somVoltar.Play();
         Invoke("CarregarCenaMenuPrincipal", somVoltar.clip.length);
-        SceneManager.LoadScene(0);
     }
 
-    public void VoltarInventario()
+    private void CarregarCenaMenuPrincipal()
     {
-        somVoltar.Play();
-        Invoke("CarregarCenaInventario", somVoltar.clip.length);
+        SceneManager.LoadScene("MenuBlueScene");
     }
 
     public void AvancaFases()
     {
         somAvancar.Play();
-        SceneManager.LoadScene(3);
-        //Invoke("CarregarCenaFases", somAvancar.clip.length);
-    }
-
-    private void CarregarCenaMenuPrincipal()
-    {
-        SceneManager.LoadScene(0);
-    }
-
-    private void CarregarCenaInventario()
-    {
-        SceneManager.LoadScene(2);
+        Invoke("CarregarCenaFases", somAvancar.clip.length);
     }
 
     private void CarregarCenaFases()
     {
-        SceneManager.LoadScene(3);
+        SceneManager.LoadScene("MenuFases");
     }
+
     
-    public void FaseMula(){
-        SceneManager.LoadScene(1);
-        Jogador.instance.SetControlsEnabled(true);
-        Time.timeScale = 1;
-    }
 }
